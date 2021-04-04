56361f6042c04847b44ba3a5f4e2fd09df855317
zulquanaaz <zulquanaaz47@gmail.com>
2021-04-04 17:25:21 -0400

    Profile fragment and dash fragment UI completed with menu.

--- app/src/main/java/com/example/assignment/DashActivity.java
+++ app/src/main/java/com/example/assignment/DashActivity.java
@@ -1,17 +1,21 @@
 package com.example.assignment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class DashActivity extends AppCompatActivity {
+public class DashActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public Toolbar toolbar;
     public DrawerLayout drawerLayout;
@@ -35,6 +39,42 @@ public class DashActivity extends AppCompatActivity {
         navController= Navigation.findNavController(this,R.id.nav_host_fragment_dash);
         NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
         NavigationUI.setupWithNavController(navigationView,navController);
-       // navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.nav_host_fragment_dash),drawerLayout);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setCheckable(true);
+        drawerLayout.closeDrawers();
+        int id=menuItem.getItemId();
+        switch (id){
+            case R.id.menu_home:
+                navController= Navigation.findNavController(this,R.id.nav_host_fragment_dash);
+                navController.navigate(R.id.dashFragment);
+                break;
+            case R.id.menu_profile:
+                navController= Navigation.findNavController(this,R.id.nav_host_fragment_dash);
+                navController.navigate(R.id.profileFragment);
+                break;
+            case R.id.menu_logout:
+                Toast.makeText(this,"Log Out!",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+
     }
 }
